76e930453ad9a66389ddca5e1618a770235ffa78
cn3085 <cn3085@gmail.com>
2021-04-19 10:48:52 +0900

    ResponseCheck Class Component

--- client.jsx
+++ client.jsx
@@ -3,11 +3,13 @@ const ReactDom = require("react-dom");
 const { default: NumberBaseball } = require("./components/NumberBaseball");
 
 const WordRelay = require("./components/WordRelay");
+import ResponseCheck from "./components/ResponseCheck";
 
 ReactDom.render(
   <>
     <WordRelay />
     <NumberBaseball />
+    <ResponseCheck />
   </>,
   document.querySelector("#root")
 );
